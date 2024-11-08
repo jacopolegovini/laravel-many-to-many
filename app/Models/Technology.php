964a7bb96d25e9422dfa77014176e0f223b0ad8a
jacopolegovini <legovini96@gmail.com>
2024-11-08 15:20:20 +0100

    fillable technology added

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -9,6 +9,10 @@ class Technology extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        "name",
+    ];
+
     public function projects()
     {
         return $this->belongsToMany(Project::class);
